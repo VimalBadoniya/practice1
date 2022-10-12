6fecd7062b99cbbd411041acaa79dcec4b51add2
Vimal Badoniya <107095697+VimalBadoniya@users.noreply.github.com>
2022-10-12 23:32:44 +0530

    Object to string & Using random value as key
    using random values as key to store patient details

--- main.js
+++ main.js
@@ -2,16 +2,22 @@ let submit = document.addEventListener('submit' , booking);
 
 function booking(e){
     e.preventDefault();
-    let name = document.getElementById('name-text').value;
+    let pName = document.getElementById('name-text').value;
     
-    let email = document.getElementById('email-text').value;
-    let contact = document.getElementById('contact-text').value;
-    let appDate = document.getElementById('date-text').value;
-    let appTime = document.getElementById('time-text').value;
+    let pEmail = document.getElementById('email-text').value;
+    let pContact = document.getElementById('contact-text').value;
+    let pAppDate = document.getElementById('date-text').value;
+    let pAppTime = document.getElementById('time-text').value;
 
-    localStorage.setItem('Name' , name);
-    localStorage.setItem('Email' , email);
-    localStorage.setItem('Contact' , contact);
-    localStorage.setItem('Date' , appDate);
-    localStorage.setItem('Time' , appTime);
+    let patient = {Name : pName , Email : pEmail , Contact : pContact , AppDate : pAppDate , AppTime : pAppTime};
+
+    let a = Math.random();
+
+
+    localStorage.setItem( a , JSON.stringify(patient));
+
+    
+
 }
+
+
