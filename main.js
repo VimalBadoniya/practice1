b8bbec082fd4710bc6157ebea75cff87f98cfaa4
Vimal Badoniya <107095697+VimalBadoniya@users.noreply.github.com>
2022-10-13 23:23:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 let submit = document.addEventListener('submit' , booking);
 
 function booking(e){
-    e.preventDefault();
-    let name = document.getElementById('name-text').value;
-    
-    let email = document.getElementById('email-text').value;
-    let contact = document.getElementById('contact-text').value;
-    let appDate = document.getElementById('date-text').value;
-    let appTime = document.getElementById('time-text').value;
+    e.preventDefault();  //to prevent default refreshing of the page when form is submitted
+    let pName = document.getElementById('name-text').value;
+    let pEmail = document.getElementById('email-text').value;   //to get value entered by patient
+    let pContact = document.getElementById('contact-text').value;
+    let pAppDate = document.getElementById('date-text').value;
+    let pAppTime = document.getElementById('time-text').value;
 
-    localStorage.setItem('Name' , name);
-    localStorage.setItem('Email' , email);
-    localStorage.setItem('Contact' , contact);
-    localStorage.setItem('Date' , appDate);
-    localStorage.setItem('Time' , appTime);
+    let patient = {Name : pName , Email : pEmail , Contact : pContact , AppDate : pAppDate , AppTime : pAppTime};
+    localStorage.setItem( pEmail , JSON.stringify(patient)); //local storage can store only string so convert obj to str
+
+    // in local storage , the patient details are saved in a string format ,  so first we have to convert it to object after that we can display it on the page
+    let patientStr = JSON.parse(localStorage.getItem(pEmail));  
+    
+    let PatientList =  document.getElementById('list');
+    let newPatient = document.createElement('li');
+    let patientDetails = document.createTextNode(pName + " " + pEmail + " " + pContact + " " + pAppDate + " " + pAppTime);
+    newPatient.appendChild(patientDetails);
+    PatientList.appendChild(newPatient);
 }
+
+
